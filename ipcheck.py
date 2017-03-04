ad15e638a6808376f19063660dd2e726032b1f83
Drew Burchfiel <drew.burchfiel@gmail.com>
2017-03-04 14:17:24 -0600

    took care of json decoder error when ip.json is empty

--- ipcheck.py
+++ ipcheck.py
@@ -1,14 +1,27 @@
-import requests, json, os
+import requests
+import json
+import os
 
-r = requests.get('https://api.ipify.org?format=json')
-j = json.loads(r.text)
-
-if os.path.isfile('ip.json'):
-    with open('ip.json', 'r') as file:
-        f = json.loads(file.read())
-        if (j['ip'] != f['ip']):
-            with open('ip.json', 'w+') as file:
-                file.write(json.dumps(j))
-else:
-    with open('ip.json', 'w+') as file:
-        file.write(json.dumps(j))
+if __name__ == '__main__':
+    resp = requests.get('https://api.ipify.org?format=json')
+    ipinfo = json.loads(resp.text)
+    
+    if os.path.isfile('ip.json'):
+        with open('ip.json', 'r') as file:
+            try:
+                f = json.loads(file.readline())
+                if ipinfo['ip'] != f['ip']:
+                    with open('ip.json', 'w+') as file:
+                        file.write(json.dumps(ipinfo))
+                        print("\nPublic IP: {ip}\n".format(ip=ipinfo['ip']))
+                else: 
+                    print("\nPublic IP: {ip}\n".format(ip=ipinfo['ip']))
+            except(json.decoder.JSONDecodeError):
+                with open('ip.json', 'w+') as file:
+                    file.write(json.dumps(ipinfo))
+                    print("\nPublic IP: {ip}\n".format(ip=ipinfo['ip']))
+            
+    else:
+        with open('ip.json', 'w+') as file:
+            file.write(json.dumps(ipinfo))
+            print("\nPublic IP: {ip}\n".format(ip=ipinfo['ip']))
